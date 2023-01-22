5c45333c67b95e8f7dd258c5bb0c2262dfc160e7
gemamur <33229517+gemamur@users.noreply.github.com>
2023-01-22 23:25:50 +0100

    router modified

--- src/core/router/app-router.tsx
+++ src/core/router/app-router.tsx
@@ -3,15 +3,18 @@ import { SwitchRoutes } from './routes';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { HomeScene } from '@/scenes';
 import { HeaderLayout } from '@/layout/HeaderLayout/header.layout';
+import { BodyContainerLayout } from '@/layout/BodyContainerLayout/bodycontainer.layout';
 
 export const AppRouter = () => {
     return (
       <Router>
         <HeaderLayout />
+        <BodyContainerLayout>
         <Routes>
           <Route path={SwitchRoutes.root} element={<HomeScene />} />
           <Route path="*" element={<Navigate to="/" />} />      
         </Routes>
+        </BodyContainerLayout>
       </Router>
     );
   };
